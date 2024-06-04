4ae26ff65d17e9ee1b632c411811231b0d2412be
manish <manish.yadav.EXT@nmims.edu>
2024-06-04 16:20:55 +0530

    kiran to do app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from "uuid"
 
-import confetti from 'canvas-confetti';
+// console.log(uuidV4())
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Tasktype = {
+  id: string ,
+  title : string ,
+  completed: boolean ,
+  createdAt: Date
+}
+
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const form = document.querySelector<HTMLFormElement>('#new-task-form');
+
+
+
+form?.addEventListener("submit", (e: SubmitEvent) => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return
+
+  const newTask : Tasktype= {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+addListItem(newTask );
+})
+
+
+function addListItem(task : Tasktype) {
+  const li = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox'; 
+  label.append(checkbox,task.title)
+  li.append(label)
+  list?.appendChild(li);
+  
+
+}
+
